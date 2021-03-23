5cbdd52d341f0d473bfdfc2fd9b5908f2ed993bf
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-23 14:36:12 -0700

    Update server.js to module + reformat properly

---
+++ server/server.mjs
@@ -0,0 +1,24 @@
+import express from 'express';
+import * as db from './db.mjs';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
+
+
+app.get('/sightings',
+    async (req, res) => res.json(await db.getSightings()))
+
+app.post('/sightings', async (req, res) => {
+    // add post request here
+})
+
