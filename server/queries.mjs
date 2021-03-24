cf042f24f8b29460197b81eb4ac2334463b53652
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-24 11:16:07 -0700

    Expand getSightings query to include  nickname

--- server/queries.mjs
+++ server/queries.mjs
@@ -12,13 +12,21 @@ const db = pgp()({
     port: "5432"
 })
 
-export const getSightings = async () => await db.any('SELECT * FROM sightings');
+export const getSightings = async () => (
+    await db.any('SELECT s.sighting_date, i.nickname, s.location, s.is_healthy, s.sighter_email'
+        + ' FROM sightings AS s'
+        + ' INNER JOIN individuals AS i'
+        + ' ON s.name = i.ind_id'
+        + ' ORDER BY sighting_date DESC'
+    )
+);
 
 export const addSighting = async (body) => (
     await db.one('INSERT INTO sightings'
         + ' (sighting_date, name, location, is_healthy, sighter_email)'
         + ' VALUES'
         + ' (${sighting_date}, ${name}, ${location}, ${is_healthy}, ${sighter_email})'
+        + ' ORDER BY sighting_date DESC'
         + ' RETURNING *',
         body)
 );
