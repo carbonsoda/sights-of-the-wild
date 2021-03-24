09cea6c18e22363fd141b025f83c67afdf5a251c
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-24 10:40:26 -0700

    Refactor AddSight code to include validation check before fetch-request

--- client/src/components/AddSight.js
+++ client/src/components/AddSight.js
@@ -36,53 +36,55 @@ export default function AddSight() {
     const submitForm = async (e) => {
         e.preventDefault();
 
-        let body = { dateTime, name, location, healthyChk, email };
-        console.log(body);
+        if (validate()) {
 
-        fetch('http://localhost:5000',
-            
-            {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(body)
-            })
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .catch(e => console.error(e.stack));
-        
+            const body = { dateTime, name, location, healthyChk, email };
+
+            fetch('http://localhost:5000',
+
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(body)
+                })
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(e => console.error(e.stack));
+        }
+
     }
 
     return (
         <>
             <h2>Add a sighting</h2>
 
-            <form class="add" onSubmit={submitForm}>
+            <form class="add" onSubmit={ submitForm }>
                 <label> Date & time seen:</label>
                 <input
                     type="datetime-local"
-                    onChange={ e => setDateTime(e.target.value)}
+                    onChange={ e => setDateTime(e.target.value) }
                 />
                 <label> Animal name: </label>
                 <input
                     type="text"
-                    onChange={ e => setName(e.target.value)}
+                    onChange={ e => setName(e.target.value) }
                 />
                 <label> Location: </label>
                 <input
                     type="text"
-                    onChange={ e => setLocation(e.target.value)}
+                    onChange={ e => setLocation(e.target.value) }
                 />
                 <label> Healthy </label>
                 <input
                     type="checkbox"
-                    onChange={() => setHealthyChk(!healthyChk)}
-                /> 
+                    onChange={ () => setHealthyChk(!healthyChk) }
+                />
                 <label> Contact email:</label>
                 <input
                     type="text"
-                    onChange={ e => setEmail(e.target.value)}
+                    onChange={ e => setEmail(e.target.value) }
                 />
 
                 <span>{ errors }</span>
