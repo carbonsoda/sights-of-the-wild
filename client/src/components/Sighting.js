ea44ec5fe1e6840ec232351f838cd51c4ca4eba1
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-24 11:14:58 -0700

    Add date formatting to individual sightings

--- client/src/components/Sighting.js
+++ client/src/components/Sighting.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
 export default function Sighting({ sighting }) {
-    // console.log(sighting);
-    const { sighting_date, sighting_time, individual_seen, is_healthy, location, sighter_email } = sighting;
+
+    const { nickname, is_healthy, location, sighter_email } = sighting;
+
+    const temp_date = new Date(sighting.sighting_date);
+    const date = new Intl.DateTimeFormat('default',
+        { dateStyle: 'short', timeStyle: 'short' }).format(temp_date);
 
     // TODO: add after checking {contact}'s format
-    const mailtoLink = `mailto:${sighter_email}?subject=${individual_seen} sighting`;
-
-    // TODO: merge date + time together and format appropriately
+    const mailtoLink = `mailto:${sighter_email}?subject=${nickname} sighting`;
 
     return (
         <>
             <td>
-                { sighting_date }
+                { date }
             </td>
             <td>
-                { individual_seen }
+                { nickname }
             </td>
             <td>
                 { is_healthy ? 'Yes' : 'No' }
